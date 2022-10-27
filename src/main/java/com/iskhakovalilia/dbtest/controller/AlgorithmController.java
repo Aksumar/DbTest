d4049751b7435deaafa50e0c369ec1fc680f3dbd
Lilia <iskhakovalilia@yandex.ru>
2022-10-27 13:50:22 +0200

    s[ring boot web dependency & a controller to handle signals were added

---
+++ src/main/java/com/iskhakovalilia/dbtest/controller/AlgorithmController.java
@@ -0,0 +1,19 @@
+package com.iskhakovalilia.dbtest.controller;
+
+import com.iskhakovalilia.dbtest.service.AlgoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AlgorithmController {
+    private final AlgoService algoService;
+
+    @PostMapping("/signal/{id}")
+    void handleSignal(@PathVariable Integer id) {
+        algoService.handleSignal(id);
+    }
+
+}
